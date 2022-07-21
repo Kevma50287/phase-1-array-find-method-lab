0e2860c88d258356dbd2c1114a585ff4e5a8d2b7
Kevin <magavakevin10@gmail.com>
2022-07-21 10:58:05 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const record = [
+    { year: "2015", result: ""},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+//Functions receives array of objects as argument
+const superbowlWin = (array) => {
+
+    //isWin is a function representing the boolean
+    const isWin = (object) => {
+        return object.result === 'W';
+    }
+
+    //findWin returns result of the find function 
+    const findWin = array.find(isWin)
+
+    //if findWin is not undefined, return the year property, else return undefined
+    if (findWin) {
+        return findWin.year
+    } else {
+        return undefined
+    }
+    
+}
+
+superbowlWin(record)
